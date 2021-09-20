facd2d76a1f7716340beb4d27ca1bb01b96296f2
sarifjaman <sarif.jaman111@gmail.com>
2021-09-20 23:13:39 +0600

    'ecom'

---
+++ admin/template.php
@@ -0,0 +1,50 @@
+<?php include('includes/head.php'); ?>
+
+<body>
+    <div class="fixed-button">
+        <a href="https://codedthemes.com/item/gradient-able-admin-template" target="_blank" class="btn btn-md btn-primary">
+            <i class="fa fa-shopping-cart" aria-hidden="true"></i> Upgrade To Pro
+        </a>
+    </div>
+    <!-- Pre-loader start -->
+    <div class="theme-loader">
+        <div class="loader-track">
+            <div class="loader-bar"></div>
+        </div>
+    </div>
+    <!-- Pre-loader end -->
+    <div id="pcoded" class="pcoded">
+        <div class="pcoded-overlay-box"></div>
+        <div class="pcoded-container navbar-wrapper">
+
+            <?php include_once('includes/header-nav.php'); ?>
+
+            <div class="pcoded-main-container">
+                <div class="pcoded-wrapper">
+
+                    <?php include_once('includes/sidenav.php'); ?>
+
+                    <div class="pcoded-content">
+                        <div class="pcoded-inner-content">
+                            <div class="main-body">
+                                <div class="page-wrapper">
+
+                                    <div class="page-body">
+                                        <div class="row">
+
+                                            <?php include('views/dashboard-view.php'); ?>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Warning Section Ends -->
+    <?php include('includes/script.php'); ?>
